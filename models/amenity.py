8eb7584accf6f4c6dea3f6cf98a604eb1bc767b6
Bakioui Souhail <58438247+souhailbakioui@users.noreply.github.com>
2024-04-20 15:17:20 +0100

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,23 +1,16 @@
 #!/usr/bin/python3
-"""Defines the Amenity class."""
-from models.base_model import Base
-from models.base_model import BaseModel
-from sqlalchemy import Column
-from sqlalchemy import String
+"""Module for Amenity class."""
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel, Base):
-    """Represents an Amenity for a MySQL database.
+    """Amenity class that inherits from BaseModel and Base."""
+    __tablename__ = 'amenities'
 
-    Inherits from SQLAlchemy Base and links to the MySQL table amenities.
-
-    Attributes:
-        __tablename__ (str): The name of the MySQL table to store Amenities.
-        name (sqlalchemy String): The amenity name.
-        place_amenities (sqlalchemy relationship): Place-Amenity relationship.
-    """
-    __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary="place_amenity",
-                                   viewonly=False)
+
+    # Establishing Many-to-Many relationship with Place
+    place_amenities = relationship("Place", secondary="place_amenity")
+
